14b5e9d1a1c6f50ec41c7295c98aaabd432f9090
dust <pawn2018@163.com>
2021-11-01 18:17:09 +0800

    提交jdk工程初始化环境文件

---
+++ jdk-core/src/test/java/com/dust/jdk/JdkCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dust.jdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdkCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
