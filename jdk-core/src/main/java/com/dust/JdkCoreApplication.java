9542eaff786d5935dd04adb064a49c786aaa13d9
dust <pawn2018@163.com>
2022-01-11 21:57:19 +0800

    add：aio demo类

--- jdk-core/src/main/java/com/dust/JdkCoreApplication.java
+++ jdk-core/src/main/java/com/dust/JdkCoreApplication.java
@@ -1,4 +1,4 @@
-package com.dust.jdk;
+package com.dust;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ jdk-core/src/main/java/com/dust/io/aio/AIOClient.java
@@ -0,0 +1,21 @@
+package com.dust.io.aio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * @author DUST
+ * @description NIO客户端
+ * @date 2022/1/7
+ */
+public class AIOClient {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 83);
+        OutputStream out = socket.getOutputStream();
+        String s = "hello world";
+        out.write(s.getBytes());
+        out.close();
+    }
+}
