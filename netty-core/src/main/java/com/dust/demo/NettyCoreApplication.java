ec1db07be3ff93c272dcdf97a9c3da103f2d8c17
dust <pawn2018@163.com>
2022-01-14 19:18:36 +0800

    add：基于netty实现web网页版聊天

--- netty-core/src/main/java/com/dust/demo/NettyCoreApplication.java
+++ netty-core/src/main/java/com/dust/demo/NettyCoreApplication.java
@@ -1,13 +1,24 @@
 package com.dust.demo;
 
+import com.dust.demo.webChat.netty.NettyWebSocketServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NettyCoreApplication {
+public class NettyCoreApplication implements CommandLineRunner {
+
+    @Autowired
+    NettyWebSocketServer nettyWebSocketServer;
 
     public static void main(String[] args) {
         SpringApplication.run(NettyCoreApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        new Thread(nettyWebSocketServer).start();
     }
 
 }
